d8947ffa43c2c10b05a195d6c3697a8eb74ec9d3
Daniel Zadok <zadok.daniel@gmail.com>
2023-01-30 21:23:31 +0200

    javascript

---
+++ W290922ER - 2023.01.30/script.js
@@ -0,0 +1,43 @@
+// comment from // to end of line
+/* comment
+from /* to 
+*/
+
+// string - literal
+"abc";
+"555";
+"2";
+"red";
+"#ddd";
+"<html></html>";
+
+// number - literal
+5 - 0;
+0;
+8;
+4.34564;
+NaN; // Not a Number
+
+// boolean - literal
+true;
+false;
+
+// null - literal (no value used by the developer)
+null;
+
+// undefined -literal (no value used by js)
+undefined;
+
+/**** Expression */
+5 + 5; // Number(10)
+5 * 8; // Number(40)
+0.1 + 0.2;
+
+/**** log function using literals*/
+console.log(5, 10, "abc", true, false, null, undefined);
+
+/**** log function using literals*/
+console.log(5 + 5);
+console.log(console.log());
+console.log(Math.PI);
+console.log(Math.sin(0.3));
