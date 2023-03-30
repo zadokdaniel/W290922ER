68daa453b8083f88efeb049aa96674db1263e5bc
Daniel Zadok <zadok.daniel@gmail.com>
2023-03-30 21:45:51 +0300

    add feedback in todo operations

--- W290922ER - 2023.03.30/script.js
+++ W290922ER - 2023.03.30/script.js
@@ -13,22 +13,35 @@ function getTodos() {
 }
 
 function addTodo(title) {
-  todos.push({
+  const newTodo = {
     title,
     // title: title,
     isComplete: false,
     createdAt: new Date(),
-  });
+  };
+
+  todos.push(newTodo);
+
+  return newTodo;
 }
 
 function removeTodo(index) {
-  todos.splice(index, 1);
+  const removed = todos.splice(index, 1)[0];
+
+  return removed ? removed : null;
 }
 
 function removeAllTodos() {
   todos.splice(0, todos.length);
+
+  return getTodos();
 }
 
 function changeTodoIsComplete(index) {
+  if (index < 0 || index > todos.length - 1) {
+    return null;
+  }
+
   todos[index].isComplete = !todos[index].isComplete;
+  return todos[index];
 }
