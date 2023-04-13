23dd1a8bae2fe7b12518800228d64b13a276ebca
Daniel Zadok <zadok.daniel@gmail.com>
2023-04-13 19:38:30 +0300

    todo app - split into different scripts (basic version)

---
+++ W290922ER - 2023.04.13/todo.js
@@ -0,0 +1,45 @@
+const todos = [
+  // {
+  //     title: 'buy milk',
+  //     isComplete: false,
+  //     createdAt: new Date()
+  // }
+];
+
+function getTodos() {
+  return todos;
+}
+
+function addTodo(title) {
+  const newTodo = {
+    title,
+    // title: title,
+    isComplete: false,
+    createdAt: new Date(),
+  };
+
+  todos.push(newTodo);
+
+  return newTodo;
+}
+
+function removeTodo(index) {
+  const removed = todos.splice(index, 1)[0];
+
+  return removed ? removed : null;
+}
+
+function removeAllTodos() {
+  todos.splice(0, todos.length);
+
+  return getTodos();
+}
+
+function changeTodoIsComplete(index) {
+  if (index < 0 || index > todos.length - 1) {
+    return null;
+  }
+
+  todos[index].isComplete = !todos[index].isComplete;
+  return todos[index];
+}
