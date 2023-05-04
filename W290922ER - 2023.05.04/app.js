1db22632d55a3f0746957649517139ff8a28f79f
Daniel Zadok <zadok.daniel@gmail.com>
2023-05-04 21:30:11 +0300

    call stack, throw try catch

---
+++ W290922ER - 2023.05.04/app.js
@@ -0,0 +1,109 @@
+import { addTodo, changeTodoIsComplete, getTodos, removeTodo } from "./todo.js";
+
+const todoInput = document.getElementById("todo-input");
+const todoAddBtn = document.getElementById("todo-add-btn");
+const todoList = document.getElementById("todo-list");
+
+renderTodoApp();
+
+todoAddBtn.addEventListener("click", handleAddTodo);
+todoInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    handleAddTodo();
+  }
+});
+
+todoList.addEventListener("change", (e) => {
+  const todoId = getTodoIdByElement(e.target);
+
+  if (todoId && e.target.matches("input.is-complete")) {
+    handleIsCompleteChange(todoId);
+  }
+});
+
+todoList.addEventListener("click", (e) => {
+  const todoId = getTodoIdByElement(e.target);
+
+  if (todoId && e.target.matches(".todo-remove-btn")) {
+    handleRemoveTodo(todoId);
+  }
+});
+
+function getTodoIdByElement(element) {
+  const todoItemElement = element.closest("li.todo-item");
+  return todoItemElement ? Number(todoItemElement.dataset.todoId) : null;
+}
+
+function handleAddTodo() {
+  addTodo(todoInput.value);
+  todoInput.value = "";
+
+  renderTodoApp();
+}
+
+function handleIsCompleteChange(id) {
+  changeTodoIsComplete(id);
+  renderTodoApp();
+}
+
+function handleRemoveTodo(id) {
+  removeTodo(id);
+  renderTodoApp();
+}
+
+function renderTodoApp() {
+  const todos = getTodos();
+
+  todoList.innerHTML =
+    todos.length > 0 ? renderTodoList(todos) : renderEmptyListNotification();
+}
+
+function renderEmptyListNotification() {
+  return `
+          <div class="bg-light rounded text-center p-3">
+            <h1 class="display-3 fw-bold">Well Done!!</h1>
+            <p>You are all done for today, go titfanen on the beach!</p>
+          </div>
+  `;
+}
+
+function renderTodoItem(todo) {
+  return `
+        <li
+          data-todo-id="${todo.id}" 
+          class="todo-item list-group-item d-flex justify-content-between"
+        >
+          <input
+            class="is-complete form-check-input me-1"
+            type="checkbox"
+            id="todo-id-${todo.id}"
+            ${todo.isComplete ? "checked" : ""}
+          />
+
+          <label
+            class="form-check-label mx-1 flex-fill ${
+              todo.isComplete
+                ? "text-secondary text-decoration-line-through"
+                : ""
+            }"
+            for="todo-id-${todo.id}"
+          >
+            ${todo.title}
+          </label>
+          <i 
+            class="todo-remove-btn text-danger bi bi-trash" 
+          >
+          </i>
+        </li>`;
+}
+
+function renderTodoList(todos = []) {
+  let html = '<ul class="list-group">';
+
+  for (const todo of todos) {
+    html += renderTodoItem(todo);
+  }
+  html += `</ul>`;
+
+  return html;
+}
---
+++ W290922ER - 2023.05.04/todo.js
@@ -0,0 +1,84 @@
+const TODO_CURRENT_ID_LS_KEY = "todo-current-id";
+const TODOS_LS_KEY = "todos";
+
+const savedCurrentId = localStorage.getItem(TODO_CURRENT_ID_LS_KEY);
+let currentId = savedCurrentId ? Number(savedCurrentId) : 1;
+
+const savedTodos = localStorage.getItem(TODOS_LS_KEY);
+let todos = savedTodos
+  ? JSON.parse(savedTodos, (key, value) => {
+      if (key === "createdAt") {
+        return new Date(value);
+      }
+
+      return value;
+    })
+  : [
+      // {
+      //     title: 'buy milk',
+      //     isComplete: false,
+      //     createdAt: new Date()
+      // }
+    ];
+
+function save() {
+  localStorage.setItem(TODOS_LS_KEY, JSON.stringify(todos));
+  localStorage.setItem(TODO_CURRENT_ID_LS_KEY, currentId);
+}
+
+export function getTodos() {
+  return todos;
+}
+
+export function addTodo(title) {
+  const newTodo = {
+    id: currentId++,
+    title,
+    // title: title,
+    isComplete: false,
+    createdAt: new Date(),
+  };
+
+  todos = [...todos, newTodo];
+
+  save();
+  return newTodo;
+}
+
+export function getTodoById(id) {
+  const todo = todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    throw new Error("the id provided is unknown");
+  }
+
+  return todo;
+}
+
+export function removeTodo(id) {
+  const removed = getTodoById(id);
+  todos = todos.filter((todo) => todo.id !== id);
+
+  save();
+  return removed;
+}
+
+export function removeAllTodos() {
+  todos = [];
+
+  save();
+  return getTodos();
+}
+
+export function changeTodoIsComplete(id) {
+  todos = todos.map((todo) => {
+    if (todo.id === id) {
+      return { ...todo, isComplete: !todo.isComplete };
+    }
+
+    return todo;
+  });
+
+  save();
+  return getTodoById(id);
+}
