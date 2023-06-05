6a5edd471dffce8a4ba38c77f513190e01690c85
Daniel Zadok <zadok.daniel@gmail.com>
2023-06-05 21:26:43 +0300

    prototype and new

--- W290922ER - 2023.06.05/script.js
+++ W290922ER - 2023.06.05/script.js
@@ -1,20 +1,37 @@
-function drive(/* this, */) {
-  console.log("drive", this);
+/**
+ * @constructor
+ */
+
+function Car(/* this, */ color) {
+  this.color = color;
+  this.tank = 0;
+  this.km = 0;
 }
 
-function createCar(/* this, */ color) {
-  return {
-    color,
-    tank: 0,
-    km: 0,
-    drive,
-    fuel() {
-      console.log("fuel", this);
-    },
-  };
-}
+Car.prototype.fuel = function fuel(/* this, */ liters) {
+  if (this.tank + liters <= 25) {
+    this.tank += liters;
+  }
+};
 
-const car1 = createCar("red");
+Car.prototype.drive = function drive(/* this, */ km) {
+  const burn = km * 0.2;
+  if (this.tank - burn >= 0) {
+    this.km += km;
+    this.tank -= burn;
+  }
+};
+
+/** instances */
+const car1 = new Car(/* { __proto__: Car.prototype }, */ "red");
+const car2 = new Car(/* { __proto__: Car.prototype }, */ "black");
+
+console.log(car1);
+car1.fuel(/*car1, */ 20);
+car1.drive(/*car1, */ 20);
 console.log(car1);
 
-car1.drive(/* car1, */);
+console.log(car2);
+car2.fuel(/*car2, */ 25);
+car2.drive(/*car2, */ 10);
+console.log(car2);
